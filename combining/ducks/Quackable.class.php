c47adb2fa57e1031591a7045443aead73550642c
raw34 <raw34@sina.com>
2013-08-01 17:06:35 +0800

    add new files

---
+++ combining/ducks/Quackable.class.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * undocumented class
+ *
+ * @package default
+ * @author raw34
+ */
+interface Quackable
+{
+    /**
+     * undocumented function
+     *
+     * @return void
+     * @author Me
+     */
+    public function quack();
+}
